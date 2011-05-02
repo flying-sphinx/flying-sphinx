dc5e4994fbc9037c7b3f802911aa9354378bdb28
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-05-02 17:23:15 +0200

    minor cleanup

--- spec/specs/configuration_spec.rb
+++ spec/specs/configuration_spec.rb
@@ -9,6 +9,7 @@ describe FlyingSphinx::Configuration do
     let(:encoded_identifier) {
       FakeWeb::Utility.encode_unsafe_chars_in_userinfo identifier
     }
+    let(:config) { FlyingSphinx::Configuration.new identifier, api_key }
     
     before :each do
       FakeWeb.register_uri(:get,
@@ -22,24 +23,20 @@ describe FlyingSphinx::Configuration do
     end
     
     it "requests details from the server with the given API key" do
-      FlyingSphinx::Configuration.new identifier, api_key
-      
+      config
       FakeWeb.should have_requested :get,
         "#{api_server}/app?api_key=#{api_key}&identifier=#{encoded_identifier}"
     end
     
     it "sets the host from the server information" do
-      config = FlyingSphinx::Configuration.new identifier, api_key
       config.host.should == 'foo.bar.com'
     end
     
     it "sets the port from the server information" do
-      config = FlyingSphinx::Configuration.new identifier, api_key
       config.port.should == 9319
     end
     
     it "sets the port from the server information" do
-      config = FlyingSphinx::Configuration.new identifier, api_key
       config.database_port.should == 10001
     end
   end
