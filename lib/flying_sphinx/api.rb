a891ff8d1c5ffa026ec5c454d271b5fe4a88ffec
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-05-05 16:47:47 +0200

    added some basic logging so we can figure out why the index call isn't being made

--- lib/flying_sphinx/api.rb
+++ lib/flying_sphinx/api.rb
@@ -16,16 +16,19 @@ class FlyingSphinx::API
   end
 
   def get(path, data = {}, options = {})
+    log('get', path, data)
     connection(:json => options[:json]).get do |req|
       req.url normalize_path(path), data.merge(api_options)
     end
   end
 
   def post(path, data = {})
+    log('post', path, data)
     connection.post(normalize_path(path), data.merge(api_options))
   end
 
   def put(path, data = {})
+    log('put', path, data)
     connection.put(normalize_path(path), data.merge(api_options))
   end
 
@@ -55,5 +58,9 @@ class FlyingSphinx::API
       builder.use Faraday::Response::ParseJson if connection_options[:json]
       builder.adapter(adapter)
     end
+  end
+
+  def log(method, path, data = {}, option = {})
+    puts "#{path} #{method} request: #{data.inspect}" if ENV['VERBOSE_LOGGING']
   end
 end
