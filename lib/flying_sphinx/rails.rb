c6a19e32b061c2d615738d0717407ade5f88be33
Pat Allan <pat@freelancing-gods.com>
2013-02-24 22:50:16 +1100

    Only load the binary classes - TS v3 won't work with Rails 2.

--- lib/flying_sphinx/rails.rb
+++ lib/flying_sphinx/rails.rb
@@ -2,10 +2,6 @@ require 'action_controller/dispatcher'
 
 if ENV['FLYING_SPHINX_IDENTIFIER'] || ENV['STAGED_SPHINX_IDENTIFIER']
   ActionController::Dispatcher.to_prepare :flying_sphinx do
-    if ThinkingSphinx::Configuration.instance.respond_to?(:settings)
-      FlyingSphinx::SphinxQL.load
-    else
-      FlyingSphinx::Binary.load
-    end
+    FlyingSphinx::Binary.load
   end
 end
