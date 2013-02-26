d0aa16f6904e78ce16aaf001cb3c92f6c91b0f46
Pat Allan <pat@freelancing-gods.com>
2013-02-26 22:26:30 +1100

    No need for a delayed processor for SphinxQL.
    ts-delayed-delta does exactly what we need, and with the FlyingSphinx::Controller in place, it should just work automatically in any environment.

--- lib/flying_sphinx/sphinxql.rb
+++ lib/flying_sphinx/sphinxql.rb
@@ -1,8 +1,6 @@
 module FlyingSphinx::SphinxQL
   def self.load
     require 'flying_sphinx/sphinxql/translator'
-    require 'flying_sphinx/sphinxql/delayed_delta'
-    require 'flying_sphinx/sphinxql/flag_as_deleted_job'
 
     FlyingSphinx.translator = Translator.new FlyingSphinx::Configuration.new
   end
