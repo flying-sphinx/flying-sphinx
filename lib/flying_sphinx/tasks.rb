6dcf10b9d7475d775bc91e1a3c3cad9de97f33e8
Pat Allan <pat@freelancing-gods.com>
2013-03-07 21:43:45 +1100

    Rake task descriptions.

--- lib/flying_sphinx/tasks.rb
+++ lib/flying_sphinx/tasks.rb
@@ -1,25 +1,26 @@
 namespace :fs do
+  desc "Upload Sphinx configuration and process indices"
   task :index => :environment do
     FlyingSphinx::CLI.new('setup').run
   end
 
+  desc "Start the Sphinx daemon on Flying Sphinx servers"
   task :start => :environment do
     FlyingSphinx::CLI.new('start').run
   end
 
+  desc "Stop the Sphinx daemon on Flying Sphinx servers"
   task :stop  => :environment do
     FlyingSphinx::CLI.new('stop').run
   end
 
+  desc "Restart the Sphinx daemon on Flying Sphinx servers"
   task :restart => :environment do
     FlyingSphinx::CLI.new('restart').run
   end
 
+  desc "Stop, configure, index and then start Sphinx"
   task :rebuild => :environment do
     FlyingSphinx::CLI.new('rebuild').run
-  end
-
-  task :index_log => :environment do
-    FlyingSphinx::IndexRequest.output_last_index
   end
 end
