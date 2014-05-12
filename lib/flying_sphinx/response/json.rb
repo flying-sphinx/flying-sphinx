7c2aa8331dc760da87696ec90872e17278d0697e
Pat Allan <pat@freelancing-gods.com>
2014-05-12 11:49:04 +1000

    Stick with hash syntax for Faraday middleware syntax.
    It works for both Faraday 0.8 and 0.9.

--- lib/flying_sphinx/response/json.rb
+++ lib/flying_sphinx/response/json.rb
@@ -1,5 +1,5 @@
 class FlyingSphinx::Response::JSON < Faraday::Response::Middleware
   def on_complete(environment)
-    environment.body = MultiJson.load environment.body
+    environment[:body] = MultiJson.load environment[:body]
   end
 end
