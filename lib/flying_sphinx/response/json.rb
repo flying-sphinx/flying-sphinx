3d6bfd494ffd730c3f09173b698859fbcfed428e
Pat Allan <pat@freelancing-gods.com>
2014-05-19 00:08:37 +1000

    Don't expect JSON for blank response bodies.

--- lib/flying_sphinx/response/json.rb
+++ lib/flying_sphinx/response/json.rb
@@ -1,5 +1,7 @@
 class FlyingSphinx::Response::JSON < Faraday::Response::Middleware
   def on_complete(environment)
+    return if environment[:request_headers]['Authorization'].nil?
+
     environment[:body] = MultiJson.load environment[:body]
   end
 end
