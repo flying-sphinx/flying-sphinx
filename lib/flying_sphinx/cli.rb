97bc87dce26ed82008fb2d5090a552ea2f6d27b5
Pat Allan <pat@freelancing-gods.com>
2018-01-08 13:26:15 +1100

    Deprecate CLI, invoke TS tasks instead.

--- lib/flying_sphinx/cli.rb
+++ lib/flying_sphinx/cli.rb
@@ -1,94 +1,31 @@
-require 'forwardable'
-
 class FlyingSphinx::CLI
-  extend Forwardable
-
   COMMANDS = {
-    'configure'  => [:configure],
-    'index'      => [:index],
-    'setup'      => [:configure, :index],
-    'start'      => [:start],
-    'stop'       => [:stop],
-    'restart'    => [:restart],
-    'rebuild'    => [:rebuild],
-    'regenerate' => [:regenerate]
+    'configure'  => "ts:configure",
+    'index'      => "ts:index",
+    'setup'      => "ts:index",
+    'start'      => "ts:start",
+    'stop'       => "ts:stop",
+    'restart'    => "ts:restart",
+    'rebuild'    => "ts:rebuild",
+    'regenerate' => "ts:rebuild"
   }
-
-  def_delegators :controller, :start, :stop, :restart
 
   def initialize(command, arguments = [])
     @command, @arguments = command, arguments
   end
 
   def run
-    methods = COMMANDS[@command]
-    raise "Unknown command #{@command}" if methods.nil?
+    task = COMMANDS[@command]
+    raise "Unknown command #{@command}" if task.nil?
 
-    methods.all? do |method|
-      FlyingSphinx.logger.info "Executing Action: #{method}"
-      result = send method
-      FlyingSphinx.logger.info "Action Finished: #{method}"
+    puts <<-MESSAGE
+The flying_sphinx CLI tool is now deprecated. Please use the standard Thinking
+Sphinx tasks instead. (Thinking Sphinx tasks will now invoke the appropriate
+behaviour for both local and Flying Sphinx environments). In this case:
 
-      result
-    end
-  end
+    heroku run rake #{task}
+        MESSAGE
 
-  private
-
-  def configuration
-    @configuration ||= FlyingSphinx::Configuration.new
-  end
-
-  def configure
-    if @arguments.empty?
-      load_rails
-
-      controller.configure
-    else
-      controller.configure File.read(@arguments.first)
-    end
-
-    true
-  end
-
-  def controller
-    @controller ||= FlyingSphinx::Controller.new configuration.api
-  end
-
-  def index
-    indices = @arguments + [{:verbose => true}]
-    controller.index *indices
-  end
-
-  def load_rails
-    return unless ENV['RAILS_ENV']
-
-    boot = File.expand_path('config/boot', Dir.pwd)
-    return unless File.exist?("#{boot}.rb")
-    require boot
-
-    if defined?(Rails) && !defined?(Rails::Railtie)
-      require File.expand_path('config/environment', Dir.pwd)
-      require 'flying_sphinx/rails'
-
-      FlyingSphinx::Binary.load
-    else
-      require File.expand_path('config/application', Dir.pwd)
-      require 'flying_sphinx/railtie'
-
-      Rails.application.require_environment!
-    end
-  end
-
-  def rebuild
-    load_rails
-
-    controller.rebuild
-  end
-
-  def regenerate
-    load_rails
-
-    controller.regenerate
+    Rake::Task[task].invoke
   end
 end
