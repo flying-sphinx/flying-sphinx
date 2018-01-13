9272b8bd5fc796099c4c178294b8d112669dad4f
Pat Allan <pat@freelancing-gods.com>
2018-01-14 00:22:20 +1100

    Start and Stop commands with specs.

---
+++ spec/flying_sphinx/commands/start_spec.rb
@@ -0,0 +1,21 @@
+require "spec_helper"
+
+RSpec.describe FlyingSphinx::Commands::Start do
+  let(:subject) do
+    FlyingSphinx::Commands::Start.new double, :api => api
+  end
+  let(:api)          { double 'API', :identifier => 'foo', :post => true }
+  let(:action_class) { double }
+
+  before :each do
+    stub_const 'FlyingSphinx::Action', action_class
+    action_class.stub(:perform) { |identifier, &block| block.call }
+  end
+
+  it "sends through action" do
+    expect(api).to receive(:post).with "/perform",
+      :action => "start"
+
+    subject.call
+  end
+end
