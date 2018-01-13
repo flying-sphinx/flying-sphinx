0343ceac9bbec28775d28c6cb907a986aff02296
Pat Allan <pat@freelancing-gods.com>
2018-01-14 00:18:33 +1100

    Index command with specs.

--- spec/flying_sphinx/commands/configure_spec.rb
+++ spec/flying_sphinx/commands/configure_spec.rb
@@ -1,15 +1,16 @@
 require "spec_helper"
 
 RSpec.describe FlyingSphinx::Commands::Configure do
-  let(:subject) { FlyingSphinx::Commands::Configure.new :api => api }
-  let(:api)     { double 'API', :identifier => 'foo', :post => true }
-  let(:action_class)  { double }
+  let(:subject) do
+    FlyingSphinx::Commands::Configure.new double, :api => api
+  end
+  let(:api)          { double 'API', :identifier => 'foo', :post => true }
+  let(:action_class) { double }
 
   before :each do
     stub_const 'FlyingSphinx::Action', action_class
     action_class.stub(:perform) { |identifier, &block| block.call }
 
-    stub_const 'FlyingSphinx::API', double(:new => api)
     stub_const 'FlyingSphinx::Configurer', double(:call => "/foo")
   end
 
