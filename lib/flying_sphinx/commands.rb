d612245e0cfcfa4ee7aa4795418ca19d3f9dce0d
Pat Allan <pat@freelancing-gods.com>
2018-03-12 10:57:24 +1100

    Add merge command.

--- lib/flying_sphinx/commands.rb
+++ lib/flying_sphinx/commands.rb
@@ -5,6 +5,7 @@ require "flying_sphinx/commands/base"
 require "flying_sphinx/commands/clear"
 require "flying_sphinx/commands/configure"
 require "flying_sphinx/commands/index_sql"
+require "flying_sphinx/commands/merge"
 require "flying_sphinx/commands/prepare"
 require "flying_sphinx/commands/rebuild"
 require "flying_sphinx/commands/reset"
@@ -21,6 +22,7 @@ ThinkingSphinx::Commander.registry.merge!(
   :clear_sql       => FlyingSphinx::Commands::Clear,
   :configure       => FlyingSphinx::Commands::Configure,
   :index_sql       => FlyingSphinx::Commands::IndexSQL,
+  :merge           => FlyingSphinx::Commands::Merge,
   :prepare         => FlyingSphinx::Commands::Prepare,
   :rebuild         => FlyingSphinx::Commands::Rebuild,
   :reset           => FlyingSphinx::Commands::Reset,
