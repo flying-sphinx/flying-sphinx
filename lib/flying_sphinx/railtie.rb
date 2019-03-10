1882ade1816f87d12f5d6df0955024fdc6bf87d3
Pat Allan <pat@freelancing-gods.com>
2019-03-10 22:44:51 +1100

    Fix configuration insertion with Thinking Sphinx v4.2.
    The new config preparation callback in Thinking Sphinx's Railtie was overwriting the saved settings from Flying Sphinx, and thus the non-localhost connection and specific port were being forgotten. So, now we've got the same callback defined here in Flying Sphinx. The order will be consistent of TS before FS, because FS loads TS as a dependency (thus, the order in an app's Gemfile is irrelevant).

--- lib/flying_sphinx/railtie.rb
+++ lib/flying_sphinx/railtie.rb
@@ -1,13 +1,29 @@
 class FlyingSphinx::Railtie < Rails::Railtie
   rake_tasks do
+    next unless FlyingSphinx::Railtie.remote_sphinx?
+
     require 'flying_sphinx/commands'
 
     ThinkingSphinx.rake_interface = FlyingSphinx::RakeInterface
 
     load File.expand_path('../tasks.rb', __FILE__)
-  end if ENV['FLYING_SPHINX_IDENTIFIER'] || ENV['STAGED_SPHINX_IDENTIFIER']
+  end
 
   initializer "flying_sphinx.set_sphinx_host_and_port" do |app|
+    FlyingSphinx::Railtie.set_up_remote_connection
+  end
+
+  config.to_prepare do
+    FlyingSphinx::Railtie.set_up_remote_connection
+  end
+
+  def self.remote_sphinx?
+    ENV['FLYING_SPHINX_IDENTIFIER'] || ENV['STAGED_SPHINX_IDENTIFIER']
+  end
+
+  def self.set_up_remote_connection
+    return unless remote_sphinx?
+
     configuration = FlyingSphinx::Configuration.new
 
     ThinkingSphinx::Configuration.instance.settings['connection_options'] = {
@@ -15,5 +31,5 @@ class FlyingSphinx::Railtie < Rails::Railtie
       :port     => 9306,
       :username => configuration.username
     }
-  end if ENV['FLYING_SPHINX_IDENTIFIER'] || ENV['STAGED_SPHINX_IDENTIFIER']
+  end
 end
